d9c1b32aed6aa4652f1621a7fae2cfe2953d9192
Marcel Huber <marcel.huber@hsr.ch>
2017-08-08 17:00:27 +0200

    replacing (?) escaped (numbered) list symbols with originals
    - even the items are placed within xml comments, gitlab fails to
      render such a block and displays some raw text instead of hiding it

--- inline_solutions.py
+++ inline_solutions.py
@@ -9,20 +9,22 @@ from subprocess import call, PIPE, Popen
 import re
 import json
 
-SOL_RE = re.compile(r'^<!--\sSOLUTION\s+(?P<contents>.*)\s*-->$', re.MULTILINE |
+SOL_RE = re.compile(r'^<!--\sSOLUTION\s+(?P<contents>.*)\s*(SOLUTION\s)?-->$', re.MULTILINE |
                     re.UNICODE | re.DOTALL)
-
+LISTNUM_ESCAPE_RE = r'\?(?P<ltype>(-|\d+\.)\s+)'
 
 def solution(key, value, format, meta):
     if key in ['RawInline', 'RawBlock']:
         if value[0] == 'html':
             solution_match = SOL_RE.search(value[1])
             if solution_match:
+                solution_contents = solution_match.group(
+                    'contents').encode('utf8')
+                solution_contents = re.sub(LISTNUM_ESCAPE_RE, lambda mo: mo.group('ltype'), solution_contents)
                 p = Popen(['pandoc', '-f', 'markdown', '-t', 'json'],
                           stdin=PIPE,
                           stdout=PIPE)
-                output, _ = p.communicate(solution_match.group(
-                    'contents').encode('utf8'))
+                output, _ = p.communicate(solution_contents)
                 out_json = json.loads(output.decode('utf8'))[1]
                 if key == 'RawBlock':
                     return out_json
