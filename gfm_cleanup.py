66ad50e7d02657d1d2237a1ccecb6cef18e0a1c4
Marcel Huber <marcel.huber@hsr.ch>
2017-09-15 17:00:39 +0200

    also convert for html5 output (wkhtmltopdf)

--- gfm_cleanup.py
+++ gfm_cleanup.py
@@ -13,7 +13,7 @@ STRIP_BACKTICKS_RE = re.compile(r'^`(?P<contents>.*)`$', re.MULTILINE |
 
 def cleanup(k, v, fmt, meta):
     if k in ['Math']:
-        if fmt in ['latex', 'beamer', 'json']:
+        if fmt in ['latex', 'beamer', 'json', 'html5']:
             math_type = v[0].get('t', None)
             if math_type in [u'InlineMath'] and len(v) > 1:
                 solution_match = STRIP_BACKTICKS_RE.search(v[1])
